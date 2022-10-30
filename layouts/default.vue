3d932f4d8f72626814aa8713a57b967423a2c592
Xyrai <Xyraidev@gmail.com>
2022-10-30 07:27:56 +0100

    Added tailwind

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <header>
+      <nav>Test</nav>
+    </header>
+    <main>
+      <slot />
+    </main>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="text-2xl text-red-400">Basic tailwind setup</div>
+</template>
