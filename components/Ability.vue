e46eafd99ec9f446cce745deab6005449a6c368b
Xyrai <Xyraidev@gmail.com>
2022-11-29 22:41:30 +0100

    Animator <3

--- components/Ability.vue
+++ components/Ability.vue
@@ -5,7 +5,7 @@
   >
     <button
       class="pb-1 transition-transform duration-500"
-      :class="active ? '-translate-y-3' : null"
+      :class="active ? '-translate-y-3' : 'hover:-translate-y-2'"
     >
       <img
         src="@/assets/img/artist-ultimate-1.png"
@@ -15,11 +15,10 @@
       />
     </button>
     <div
-      v-if="active"
-      class="h-5 mt-3 w-[1px] bg-[#D0A85C] transition-transform duration-500"
-      :class="active ? 'scale-y-100' : 'scale-y-0'"
+      class="h-5 w-[1px] bg-[#D0A85C] transition-all duration-500"
+      :class="active ? 'scale-y-100 opacity-100' : 'scale-y-0 opacity-0'"
     ></div>
-    <Bullet :active="active" :class="active ? '-mb-[10px]' : '-mb-1 mt-5'" />
+    <Bullet :active="active" :class="active ? '-mb-[10px]' : '-mb-1'" />
   </li>
 </template>
 
