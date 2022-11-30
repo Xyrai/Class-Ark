513d4c849a828b7f909e2fe06f8d53bc4c04b9f2
Xyrai <Xyraidev@gmail.com>
2022-11-30 19:01:41 +0100

    Added font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,12 @@ export default defineNuxtConfig({
   target: "static",
   modules: ["@nuxtjs/tailwindcss", "@nuxt/image-edge"],
   image: {},
+  head: {
+    link: [
+      {
+        rel: "stylesheet",
+        href: "https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600;1,700&display=swap",
+      },
+    ],
+  },
 });
